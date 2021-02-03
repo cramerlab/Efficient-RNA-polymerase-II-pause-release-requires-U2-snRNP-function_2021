ec026cf127a58e5dacb118b75bd0a4fc7bb6d373
Sara Monteiro Martins <sara.martins@mpibpc.mpg.de>
2021-02-03 21:50:35 +0100

    Create UniqueTranscribedBasesCoverageRleTracks.R

---
+++ R_scripts/UniqueTranscribedBasesCoverageRleTracks.R
@@ -0,0 +1,29 @@
+library(GenomicAlignments)
+
+load("Utils/human.chrs.RData")
+load("Utils/human.chrs.length.RData")
+
+dir.create("ProcessedData/UniqueTranscribedBasesCoverageRleTracks"))
+bam.files <- dir("Bamfiles")
+
+for (bam.file in bam.files){
+  dir.create("ProcessedData/UniqueTranscribedBasesCoverageRleTracks/bam.file")
+  build.unique.transcribed.bases.coverage.track.list = function(which.chr){
+    unique.transcribed.bases.coverage.track.list.chr = list()
+    param = ScanBamParam(which=GRanges(seqnames = which.chr,ranges = IRanges(0,human.chrs.lengths[which.chr])))
+    bam = readGAlignmentPairs(file = "Bamfiles/bam.file"),param = param)
+    bam = bam[start(left(bam)) <= end(right(bam))]
+    rle.vec = Rle(0,human.chrs.lengths[which.chr])
+    coverage.vec = coverage(GRanges(seqnames = which.chr,ranges = IRanges(start = start(left(bam[strand(bam) == "+"])),end = end(right(bam[strand(bam) == "+"])))))[[which.chr]]
+    rle.vec[1:length(coverage.vec)] = coverage.vec
+    unique.transcribed.bases.coverage.track.list.chr[["+"]] = rle.vec
+    rle.vec = Rle(0,human.chrs.lengths[which.chr])
+    coverage.vec = coverage(GRanges(seqnames = which.chr,ranges = IRanges(start = start(left(bam[strand(bam) == "-"])),end = end(right(bam[strand(bam) == "-"])))))[[which.chr]]
+    rle.vec[1:length(coverage.vec)] = coverage.vec
+    unique.transcribed.bases.coverage.track.list.chr[["-"]] = rle.vec
+    save(unique.transcribed.bases.coverage.track.list.chr,file = "ProcessedData/UniqueTranscribedBasesCoverageRleTracks/bam.file/paste0("unique.transcribed.bases.coverage.track.list.",which.chr,".RData"))
+    return()
+  }
+    unique.transcribed.bases.coverage.track.list = foreach(n = human.chrs,.noexport = setdiff(ls(),c("human.chrs.lengths"))) %dopar% build.unique.transcribed.bases.coverage.track.list(n)
+}
+  
