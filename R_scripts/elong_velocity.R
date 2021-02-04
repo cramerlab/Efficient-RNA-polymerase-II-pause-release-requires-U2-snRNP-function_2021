6bfad9357c306d0c908a2f3138c0c08b9f7a6bbb
Sara Monteiro Martins <sara.martins@mpibpc.mpg.de>
2021-02-04 02:16:22 +0100

    Update elong_velocity.R

--- R_scripts/elong_velocity.R
+++ R_scripts/elong_velocity.R
@@ -2,7 +2,7 @@ load("AnnotationObjects/human.refseq.major.isoform.exon.RData")
 load("AnnotationObjects/human.refseq.major.isoform.intron.RData")
 load("AnnotationObjects/protein.coding.RData")
 
-#sequencing.depth, cross.contamination and antisense.bias.ratio calculation can be found in https://github.com/cramerlab/TT-seq_analysis/
+#sequencing.depth, cross.contamination and expressed.TR.5.50 calculation can be found in https://github.com/cramerlab/TT-seq_analysis/
 load("ProcessedData/TTseq/sequencing.depth.RData")
 load("ProcessedData/TTseq/cross.contamination.RData")
 load("ProcessedData/TTseq/expressed.TR.5.50.RData")
@@ -34,8 +34,7 @@ human.refseq.major.isoform.exon.first.non.single = human.refseq.major.isoform.ex
 exon.first.non.single = as.character(human.refseq.major.isoform.exon.first.non.single[,"id"])	
 exon.first.non.single.list = tapply(as.character(human.refseq.major.isoform.exon.first.non.single[,"id"]),INDEX = as.character(human.refseq.major.isoform.exon.first.non.single[,"transcript_id"]),identity)
 
-TTseq.human.refseq.major.isoform.exon.first.non.single.coverage.antisense.corrected = sapply(exon.first.non.single.list,function(x){sum(apply(t(t((t(t(human.refseq.major.isoform.exon.coverage.antisense.corrected.TTseq[x,c("control_1.bam","control_2.bam")])/alternative.sequencing.depth[c("control_1.tabular","control_2.tabular")])) 
-                                                                                                                                                  - t(alternative.cross.contamination[c("control_1.tabular","control_2.tabular")]*t((t(t(human.refseq.major.isoform.exon.coverage.antisense.corrected.TTseq[x,c("RNAseq_control_1.bam","RNAseq_control_2.bam")])/alternative.sequencing.depth[c("RNAseq_control_1.tabular","RNAseq_control_2.tabular")])))))/(1 - alternative.cross.contamination[c("control_1.tabular","control_2.tabular")])),1,function(x){x[x < 0] = 0;sum(x)}))})
+TTseq.human.refseq.major.isoform.exon.first.non.single.coverage.antisense.corrected = sapply(exon.first.non.single.list,function(x){sum(apply(t(t((t(t(human.refseq.major.isoform.exon.coverage.antisense.corrected.TTseq[x,c("control_1.bam","control_2.bam")])/sequencing.depth[c("control_1.tabular","control_2.tabular")])) - t(cross.contamination[c("control_1.tabular","control_2.tabular")]*t((t(t(human.refseq.major.isoform.exon.coverage.antisense.corrected.TTseq[x,c("RNAseq_control_1.bam","RNAseq_control_2.bam")])/sequencing.depth[c("RNAseq_control_1.tabular","RNAseq_control_2.tabular")])))))/(1 - cross.contamination[c("control_1.tabular","control_2.tabular")])),1,function(x){x[x < 0] = 0;sum(x)}))})
 
 mNETseq.human.refseq.major.isoform.exon.first.non.single.coverage.antisense.corrected = sapply(exon.first.non.single.list,function(x){sum(human.refseq.major.isoform.exon.coverage.antisense.corrected.mNETseq[x,c("control_1.bam","control_2.bam")]/size.factors[c("control_1.tabular","control_2.tabular")])})
 
@@ -51,8 +50,7 @@ human.refseq.major.isoform.intron.first.non.single = human.refseq.major.isoform.
 intron.first.non.single = as.character(human.refseq.major.isoform.intron.first.non.single[,"id"])	
 intron.first.non.single.list = tapply(as.character(human.refseq.major.isoform.intron.first.non.single[,"id"]),INDEX = as.character(human.refseq.major.isoform.intron.first.non.single[,"transcript_id"]),identity)
 
-TTseq.human.refseq.major.isoform.intron.first.non.single.coverage.antisense.corrected = sapply(intron.first.non.single.list,function(x){sum(apply(t(t((t(t(human.refseq.major.isoform.intron.coverage.antisense.corrected.TTseq[x,c("control_1.bam","control_2.bam")])/alternative.sequencing.depth[c("control_1.tabular","control_2.tabular")])) 
-                                                                                                                                                  - t(alternative.cross.contamination[c("control_1.tabular","control_2.tabular")]*t((t(t(human.refseq.major.isoform.intron.coverage.antisense.corrected.TTseq[x,c("RNAseq_control_1.bam","RNAseq_control_2.bam")])/alternative.sequencing.depth[c("RNAseq_control_1.tabular","RNAseq_control_2.tabular")])))))/(1 - alternative.cross.contamination[c("control_1.tabular","control_2.tabular")])),1,function(x){x[x < 0] = 0;sum(x)}))})
+TTseq.human.refseq.major.isoform.intron.first.non.single.coverage.antisense.corrected = sapply(intron.first.non.single.list,function(x){sum(apply(t(t((t(t(human.refseq.major.isoform.intron.coverage.antisense.corrected.TTseq[x,c("control_1.bam","control_2.bam")])/sequencing.depth[c("control_1.tabular","control_2.tabular")])) - t(cross.contamination[c("control_1.tabular","control_2.tabular")]*t((t(t(human.refseq.major.isoform.intron.coverage.antisense.corrected.TTseq[x,c("RNAseq_control_1.bam","RNAseq_control_2.bam")])/sequencing.depth[c("RNAseq_control_1.tabular","RNAseq_control_2.tabular")])))))/(1 - cross.contamination[c("control_1.tabular","control_2.tabular")])),1,function(x){x[x < 0] = 0;sum(x)}))})
 
 mNETseq.human.refseq.major.isoform.intron.first.non.single.coverage.antisense.corrected = sapply(intron.first.non.single.list,function(x){sum(human.refseq.major.isoform.intron.coverage.antisense.corrected.mNETseq[x,c("control_1.bam","control_2.bam")]/size.factors[c("control_1.tabular","control_2.tabular")])})
 
